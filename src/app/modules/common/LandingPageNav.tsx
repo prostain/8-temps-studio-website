db3e0323645beb71ae288e6880fe601fbaeae0a9
samir-massoundi <samir.j7.massoundi@gmail.com>
2022-03-20 19:35:28 +0100

    rebuild first step

--- src/app/modules/common/LandingPageNav.tsx
+++ src/app/modules/common/LandingPageNav.tsx
@@ -1,49 +1,41 @@
 import React from 'react'
+import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { getCurrentUser } from '../../services/auth'
+
 
 import huitTempsLogoSm from '../../../asset/Horizontal/8T-White-sm.png'
 
 export default function LandingPageNav() {
     return (
-        <nav className='landingPageNav'>
-            <div className='sideNav'>
-                <img className='imgScale' src={huitTempsLogoSm} alt="" />
-            </div>
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="/home"> <img className='imgScale' src={huitTempsLogoSm} alt="" /> </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
 
-            <div>
-                <a href="/home"> Accueil </a>
-            </div>
+                    <Nav className="mx-auto">
+                        <Nav.Link href="/home">Accueil</Nav.Link>
+                        <Nav.Link href="/concept">Concept</Nav.Link>
+                        <Nav.Link href="/concept">Catégories</Nav.Link>
+                        <Nav.Link href="/concept">Professeurs</Nav.Link>
+                        <Nav.Link href="/concept">Abonnement</Nav.Link>
+                        <Nav.Link href="/concept">Contacts</Nav.Link>
+                        <Nav.Link href="/concept">Blog</Nav.Link>
 
-            <div>
-                <a href='#'>Concept</a>
-            </div>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link>
+                            {localStorage.getItem('user') ? "Connecté" : "S'inscrire"}
+                            <i className="bi bi-person-fill light"></i>
+                            <i className="bi bi-search"></i>
+                            <svg className="bi" width="32" height="32" fill="currentColor">
+                                <use xlinkHref="bootstrap-icons.svg#heart-fill" />
+                            </svg>
+                        </Nav.Link>
+                    </Nav>
 
-
-            <div>
-                <a href='#'>Catégories</a>
-            </div>
-            <div>
-                <a href='#'>Professeurs</a>
-            </div>
-
-            <div>
-                <a href='#'>Abonnements</a>
-            </div>
-
-            <div>
-                <a href='#'>Blog</a>
-            </div>
-
-            <div>
-                <a href='#'> Contacts</a>
-            </div>
-
-            <div className='sideNav'>
-
-
-                <i className="bi bi-search"></i>
-
-            </div>
-
-        </nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
